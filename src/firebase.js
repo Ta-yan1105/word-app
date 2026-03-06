cb6d7246284d89dab35bc3ba54e220fb510855d8
Ta-yan <tadashi.nishide@gmail.com>
2026-03-06 15:03:14 +0900

    完成版

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// 👇 ここに、先ほど「コピーボタン」を押して取得したコードをそのまま貼り付けてください！
-// （以下の const firebaseConfig = { ... }; の部分をまるごと上書きします）
+// Reflection（マスターデータベース）の鍵
 const firebaseConfig = {
-  apiKey: "AIzaSyBmcXqTlsLUd-yoz-thuZymltFyUiROYUE",
-  authDomain: "my-word-app-5b588.firebaseapp.com",
-  projectId: "my-word-app-5b588",
-  storageBucket: "my-word-app-5b588.firebasestorage.app",
-  messagingSenderId: "275462943198",
-  appId: "1:275462943198:web:53d33f176699400e36ad4f",
-  measurementId: "G-YQM20K7JQ4"
+  apiKey: "AIzaSyAMvD6g3pTmneNad4-h8ZT_rzfZfn3T2YM",
+  authDomain: "my-english-log-app.firebaseapp.com",
+  projectId: "my-english-log-app",
+  storageBucket: "my-english-log-app.firebasestorage.app",
+  messagingSenderId: "693893816448",
+  appId: "1:693893816448:web:3c6bfac6dc4dffaa8a0665"
 };
 
+// アプリの初期化
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
